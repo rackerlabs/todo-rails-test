e1171213fd570582575a3855376d2c359e115967
Ryan A. Johnson <ryan.johnson@rackspace.com>
2014-03-18 13:47:14 -0500

    proper comments in configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,10 +11,14 @@ TodoRailsTest::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  # Compress JavaScripts and CSS
   config.assets.compress = true
-  config.assets.compile = true
-  config.assets.digest = true
 
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
