c95eba12e9b6508a82c74453d72da6fd6b62cc25
Michael Cooley <mikejames386@gmail.com>
2014-04-16 17:24:33 -0500

    add due date column to tasks model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140318155532) do
+ActiveRecord::Schema.define(:version => 20140416221612) do
 
   create_table "lists", :force => true do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20140318155532) do
     t.integer  "list_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.date     "due_date"
   end
 
   add_index "tasks", ["list_id"], :name => "index_tasks_on_list_id"
