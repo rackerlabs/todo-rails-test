edd753c03a66cbcdce01c47432618e23ebaf1785
Michael Cooley <mike@Jackis-Mac-mini.local>
2014-04-17 11:26:09 -0500

    add due dates to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,28 +18,33 @@ triforce = Task.new
 triforce.name = "Obtain the Triforce"
 triforce.complete = false
 triforce.list_id = 1
+triforce.due_date = "2017-02-01"
 triforce.save!
 
 master_sword = Task.new
 master_sword.name = "Find the Master Sword"
 master_sword.list_id = 1
 master_sword.complete = false
+master_sword.due_date = "2016-02-03"
 master_sword.save!
 
 defeat_gannon = Task.new
 defeat_gannon.name = "Defeat Gannon"
 defeat_gannon.list_id = 1
 defeat_gannon.complete = false
+defeat_gannon.due_date = "2013-05-07"
 defeat_gannon.save!
 
 groceries = Task.new
 groceries.name = "Go to the grocery store"
 groceries.list_id = 2
 groceries.complete = false
+groceries.due_date = "2013-04-09"
 groceries.save!
 
 bank = Task.new
 bank.name = "Go to the Bank"
 bank.list_id = 2
 bank.complete = false
+bank.due_date = "2013-04-10"
 bank.save!
