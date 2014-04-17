87b249d72f2a4a3a32267264d3b6bff2d422227f
Michael Cooley <mikejames386@gmail.com>
2014-04-17 13:41:15 -0500

    add respond to json action to lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -23,22 +23,43 @@ class ListsController < ApplicationController
   def create
     @list = List.new(params[:list])
 
-    if @list.save
-      redirect_to @list, notice: 'List was successfully created.'
-    else
-      render action: "new"
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @list }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
     end
+
+    # if @list.save
+    #   redirect_to @list, notice: 'List was successfully created.'
+    # else
+    #   render action: "new"
+    # end
   end
 
   # PUT /lists/1
   def update
     @list = List.find(params[:id])
 
-    if @list.update_attributes(params[:list])
-      redirect_to @list, notice: 'List was successfully updated.'
-    else
-      render action: "edit"
+    respond_to do |format|
+      if @list.update(list_params)
+
+        format.html { redirect_to @list, notice: 'List was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
     end
+
+    # if @list.update_attributes(params[:list])
+    #   redirect_to @list, notice: 'List was successfully updated.'
+    # else
+    #   render action: "edit"
+    # end
   end
 
   # DELETE /lists/1
@@ -46,6 +67,9 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     @list.destroy
 
-    redirect_to lists_url
+    respond_to do |format|
+      format.html { redirect_to lists_url }
+      format.json { head :no_content }
+    end
   end
 end
